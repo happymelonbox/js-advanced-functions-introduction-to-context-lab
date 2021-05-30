7169c3605c0d41d363130a866595f7b8abf97b87
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-30 22:20:02 +1000

    cannot figure createEmployeeRecords function out.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function createEmployeeRecord(array = ['','','',0]){
+        let obj = {
+            firstName: array[0],
+            familyName: array[1],
+            title: array[2],
+            payPerHour: array[3],
+            timeInEvents: [],
+            timeOutEvents: []
+}
+return obj
+}
+
+function createEmployeeRecords(array){
+    array.forEach(element => {createEmployeeRecord(element)})
+}
